0f827399594d36b8888d9c33f92392862c9b945d
gigiluu83 <gigiluu83@gmail.com>
2019-08-11 21:02:19 -0400

    initial push

---
+++ apps.js
@@ -0,0 +1,43 @@
+$("#hide1").click(function(){
+    $(".hide1").hide();
+  });
+  
+  $("#show1").click(function(){
+    $(".hide1").show();
+  });
+
+
+  $("#hide2").click(function(){
+    $(".hide2").hide();
+  });
+  
+  $("#show2").click(function(){
+    $(".hide2").show();
+  });  
+
+
+  $("#hide3").click(function(){
+    $(".hide3").hide();
+  });
+  
+  $("#show3").click(function(){
+    $(".hide3").show();
+  });  
+
+
+  $("#hide4").click(function(){
+    $(".hide4").hide();
+  });
+  
+  $("#show4").click(function(){
+    $(".hide4").show();
+  });
+
+
+  $("#hide").click(function(){
+    $(".hide").hide();
+  });
+  
+  $("#show").click(function(){
+    $(".hide").show();
+  });
